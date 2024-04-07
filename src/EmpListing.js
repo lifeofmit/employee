0d38462557f7978c7fab30b474a16632f2c426c3
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-07 12:13:50 +0530

    Remove functionality is done.

--- src/EmpListing.js
+++ src/EmpListing.js
@@ -12,7 +12,16 @@ const EmpListing = () => {
       navigate("/employee/edit/"+id);
     }
     const Removefunction=(id)=>{
-
+      if(window.confirm('Do you want to remove?')){
+        fetch("http://localhost:8000/employee/"+ id,{
+            method:"DELETE"
+        }).then((res)=>{
+            alert('Removed successfully.')
+            window.location.reload();
+        }).catch((err)=>{
+            console.log(err.message)
+        })
+      }
     }
 
     useEffect(() => {
