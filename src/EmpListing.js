c28ba24e80ef2b017ed3a434bbf85e191ac257dd
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-07 12:53:46 +0530

    Table header color correction.

--- src/EmpListing.js
+++ src/EmpListing.js
@@ -2,37 +2,42 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const EmpListing = () => {
-    const[empdata,empdatachange]=useState(null);
-    const navigate=useNavigate();  
+  const [empdata, empdatachange] = useState(null);
+  const navigate = useNavigate();
 
-    const LoadDetail=(id)=>{
-      navigate("/employee/detail/"+id);
-    }
-    const LoadEdit=(id)=>{
-      navigate("/employee/edit/"+id);
-    }
-    const Removefunction=(id)=>{
-      if(window.confirm('Do you want to remove?')){
-        fetch("http://localhost:8000/employee/"+ id,{
-            method:"DELETE"
-        }).then((res)=>{
-            alert('Removed successfully.')
-            window.location.reload();
-        }).catch((err)=>{
-            console.log(err.message)
+  const LoadDetail = (id) => {
+    navigate("/employee/detail/" + id);
+  };
+  const LoadEdit = (id) => {
+    navigate("/employee/edit/" + id);
+  };
+  const Removefunction = (id) => {
+    if (window.confirm("Do you want to remove?")) {
+      fetch("http://localhost:8000/employee/" + id, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          alert("Removed successfully.");
+          window.location.reload();
         })
-      }
+        .catch((err) => {
+          console.log(err.message);
+        });
     }
+  };
 
-    useEffect(() => {
-        fetch("http://localhost:8000/employee").then((res) => {
-            return res.json();
-        }).then((resp) => {
-            empdatachange(resp);
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:8000/employee")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
     <div className="container">
       <div className="card">
@@ -40,34 +45,57 @@ const EmpListing = () => {
           <h2>Employee Listing</h2>
         </div>
         <div className="card-body">
-            <div className="divbtn">
-                <Link to="employee/create" className="btn btn-success">Add New (+)</Link>
-            </div>
+          <div className="divbtn">
+            <Link to="employee/create" className="btn btn-success">
+              Add New (+)
+            </Link>
+          </div>
           <table className="table table-bordered">
-            <thead className="bg-dark text-white">
+            <thead>
               <tr>
-                <td>ID</td>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Action</td>
+                <th className="bg-dark text-white">ID</th>
+                <th className="bg-dark text-white">Name</th>
+                <th className="bg-dark text-white">Email</th>
+                <th className="bg-dark text-white">Phone</th>
+                <th className="bg-dark text-white">Action</th>
               </tr>
             </thead>
             <tbody>
-                { empdata &&
-                    empdata.map(item => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.phone}</td>
-                            <td>
-                                <a onClick={()=>{LoadEdit(item.id)}} className="btn btn-success">Edit</a>
-                                <a onClick={()=>{Removefunction(item.id)}} className="btn btn-danger">Remove</a>
-                                <a onClick={()=>{LoadDetail(item.id)}} className="btn btn-primary">Details</a>
-                            </td>
-                        </tr>
-                    ))
-                }
+              {empdata &&
+                empdata.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>
+                      <a
+                        onClick={() => {
+                          LoadEdit(item.id);
+                        }}
+                        className="btn btn-success"
+                      >
+                        Edit
+                      </a>
+                      <a
+                        onClick={() => {
+                          Removefunction(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        Remove
+                      </a>
+                      <a
+                        onClick={() => {
+                          LoadDetail(item.id);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        Details
+                      </a>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
